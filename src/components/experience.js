33e63153e98d21b823ebaf82b0db9aeea5856e17
nguyendangthao <nguyendangthaoit@gmail.com>
2023-01-11 14:23:14 +0800

    update exp section

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,96 +1,86 @@
 
 function Experience() {
+    const data = [
+        {
+            period: '2022 - Now',
+            title: 'SENIOR APPLICATION DEVELOPER',
+            compony: '(FPT Malaysia)',
+            desc: `Estimate and manage user stories. Communicate with end user and PM. Build and develop systems.
+            Evaluating technologies. Review and support team member's code. 
+            Use tech Net core, TypeScript, SQL, VueJS, Dev.azure.`
+        },
+        {
+            period: '2020 - 2022',
+            title: 'SENIOR FRONT END DEVELOPER',
+            compony: '(FPT Malaysia)',
+            desc: `Evaluating front-end technologies. Defined the architecture, coding convention. 
+            Developed web, mobile application. Communicated with client. Used tech .Net, Angular, ReactJS, React-Native, Dev.azure.`
+        },
+        {
+            period: '2020 - 2021',
+            title: 'SENIOR APPLICATION DEVELOPER',
+            compony: 'Freelancer, Outsource',
+            desc: `Communicated with client and got documents. Estimated and manage tasks. Defined the architecture, coding convention. 
+            Reviewed and support team member. Deployed and test application. Used tech NodeJS, MongoDB, ReactJS, React-Native, Git, Shopify
+            platform.`
+        },
+        {
+            period: '2017 - 2019',
+            title: 'FRONT END DEVELOPER',
+            compony: '(FPT Software Hanoi)',
+            desc: `Implemented front end side. Integrated API to the frontend. Wrote unit test
+            Deployed application. Communicated with tester and user. Use tech Net, SQL, Angular, AngularJS, ReactJS, Jira.`
+        },
+        {
+            period: '2015 - 2017',
+            title: 'INTERN, CODER',
+            compony: '(NACENTECH)',
+            desc: `Studied about process working.\n Researched and developed Android mobile.\n Developed desktop application.\n
+            Used tech C#, C, java.\n`
+        }
+    ];
+
     return (
         <>
             <div id="exp" className="agile-edu exp">
                 <div className="container">
                     <h3 className="w3l-heading">Experience</h3>
-                    <div className="work-info">
-                        <div className="col-md-6 col-sm-6 col-xs-6 work-right work-right2">
-                        </div>
-                        <div className="col-md-6  col-sm-6 col-xs-6  work-left work-left2">
-                            <div className="col-md-6  col-sm-6 col-xs-6  work1 detail">
-                                <h4>SENIOR FRONT END DEVELOPER</h4>
-                                <h5>(FPT Malaysia)</h5>
-                                <span>Estimate and manage user stories</span>
-                                <span>Communicate with end user and PM.</span>
-                                <span>Build and develop new systems.</span>
-                                <span>Maintain and fix production issues.</span>
-                                <span>Coach new members.</span>
-                                <span>Review and support team member's code.</span>
-                                <span> Use tech Net core, TypeScript, SQL, VueJS, Dev.azure.</span>
-
-
-                            </div>
-                            <div className="col-md-6  col-sm-6 col-xs-6  work2">
-                                <h5 className="node"> 2022 - Now</h5>
-                                <span></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="work-info">
-                        <div className="col-md-6  col-sm-6 col-xs-6 ">
-                        </div>
-                        <div className="col-md-6  col-sm-6 col-xs-6 work-right">
-                            <div className="col-md-6 col-sm-6 col-xs-6 work2">
-                                <span></span>
-                                <h5 className="node">2020-2022</h5>
-                            </div>
-                            <div className="col-md-6  col-sm-6 col-xs-6 work-right1 detail">
-                                <h4>SENIOR FRONT END DEVELOPER</h4>
-                                <h5>(FPT Malaysia)</h5>
-                                <span>Estimated and implemented user stories.</span>
-                                <span>Implemented full stack.</span>
-                                <span>Developed mobile application.</span>
-                                <span>Hot fix product bug.</span>
-                                <span>Communicated with client.</span>
-                                <span>Used tech .Net, Angular, ReactJS, React-Native, Dev.azure.</span>
-                            </div>
-                        </div>
-                        <div className="clearfix"> </div>
-                    </div>
-                    <div className="work-info">
-                        <div className="col-md-6 col-sm-6 col-xs-6 work-right work-right2">
-                        </div>
-                        <div className="col-md-6  col-sm-6 col-xs-6  work-left work-left2">
-                            <div className="col-md-6  col-sm-6 col-xs-6  work1 detail">
-                                <h4>FRONT END DEVELOPER</h4>
-                                <h5>(FPT Software Hanoi)</h5>
-                                <span>Implemented user stories.</span>
-                                <span>Implemented front end side.</span>
-                                <span>Supported another projects.</span>
-                                <span>Deployed application.</span>
-                                <span>Communicated with tester and user.</span>
-                                <span>Review and support team member's code.</span>
-                                <span>Use tech Net, SQL, Angular, AngularJS, ReactJS, Jira.</span>
-                            </div>
-                            <div className="col-md-6  col-sm-6 col-xs-6  work2">
-                                <h5 className="node"> 2017 - 2019</h5>
-                                <span></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="work-info">
-                        <div className="col-md-6  col-sm-6 col-xs-6 ">
-                        </div>
-                        <div className="col-md-6  col-sm-6 col-xs-6 work-right">
-                            <div className="col-md-6 col-sm-6 col-xs-6 work2">
-                                <span></span>
-                                <h5 className="node">2015-2017</h5>
-                            </div>
-                            <div className="col-md-6  col-sm-6 col-xs-6 work-right1 detail">
-                                <h4>INTERN, CODER</h4>
-                                <h5>(NACENTECH)</h5>
-                                <span>Studied about process working.</span>
-                                <span>Researched and developed Android mobile.</span>
-                                <span>Developed desktop application.</span>
-                                <span>Set up and tested application.</span>
-                                <span>Communicated with client.</span>
-                                <span>Used tech C#, C, java.</span>
-                            </div>
-                        </div>
-                        <div className="clearfix"> </div>
-                    </div>
+                    {
+                        data.map((item, i) =>
+                            (i % 2 === 0) ?
+                                <div className="work-info" key={i}>
+                                    <div className="col-md-6 col-sm-6 col-xs-6 work-right work-right2">
+                                    </div>
+                                    <div className="col-md-6 col-sm-6 col-xs-6 work-left work-left2" data-aos="fade-down-right">
+                                        <div className="col-md-6  col-sm-6 col-xs-6 work1 detail">
+                                            <h4>{item.title}</h4>
+                                            <h5>{item.compony}</h5>
+                                            <span>{item.desc}</span>
+                                        </div>
+                                        <div className="col-md-6 col-sm-6 col-xs-6 work2">
+                                            <h5 className="node">{item.period}</h5>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                </div> :
+                                <div className="work-info" key={i}>
+                                    <div className="col-md-6 col-sm-6 col-xs-6 ">
+                                    </div>
+                                    <div className="col-md-6 col-sm-6 col-xs-6 work-right" data-aos="fade-down-left">
+                                        <div className="col-md-6 col-sm-6 col-xs-6 work2">
+                                            <span></span>
+                                            <h5 className="node">{item.period}</h5>
+                                        </div>
+                                        <div className="col-md-6  col-sm-6 col-xs-6 work-right1 detail">
+                                            <h4>{item.title}</h4>
+                                            <h5>{item.compony}</h5>
+                                            <span>{item.desc}</span>
+                                        </div>
+                                    </div>
+                                    <div className="clearfix"> </div>
+                                </div>
+                        )
+                    }
                 </div>
             </div>
         </>
